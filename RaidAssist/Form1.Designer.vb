745a37c19b8168b139cc01bde08a5009e5218c29
lynxboi <lynxxboi@gmail.com>
2015-08-21 04:00:56 +0200

    Initial
    Initial thing

---
+++ RaidAssist/Form1.Designer.vb
@@ -0,0 +1,389 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.PictureBox1.Image = Global.RaidAssist.My.Resources.Resources.Meatspin
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, 1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(177, 119)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 48.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(176, 1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(430, 76)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "LYNXBOI"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.PictureBox2.Image = Global.RaidAssist.My.Resources.Resources.Meatspin
+        Me.PictureBox2.Location = New System.Drawing.Point(607, 1)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(177, 119)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label2.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(183, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(418, 19)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "time"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label3.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(183, 98)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(418, 19)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "StreamKey" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Panel2.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel2.Controls.Add(Me.Button14)
+        Me.Panel2.Controls.Add(Me.Button17)
+        Me.Panel2.Controls.Add(Me.Button16)
+        Me.Panel2.Controls.Add(Me.Button7)
+        Me.Panel2.Controls.Add(Me.Button15)
+        Me.Panel2.Controls.Add(Me.Button12)
+        Me.Panel2.Controls.Add(Me.Button11)
+        Me.Panel2.Controls.Add(Me.CheckBox1)
+        Me.Panel2.Controls.Add(Me.Button10)
+        Me.Panel2.Controls.Add(Me.Button9)
+        Me.Panel2.Controls.Add(Me.Button8)
+        Me.Panel2.Controls.Add(Me.Button6)
+        Me.Panel2.Controls.Add(Me.Button5)
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.Button3)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.PictureBox1)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Controls.Add(Me.PictureBox2)
+        Me.Panel2.Location = New System.Drawing.Point(0, -1)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(783, 141)
+        Me.Panel2.TabIndex = 6
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.Crimson
+        Me.Button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button14.Location = New System.Drawing.Point(312, 120)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(21, 21)
+        Me.Button14.TabIndex = 18
+        Me.Button14.Text = "X"
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button12.Location = New System.Drawing.Point(179, 120)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(36, 21)
+        Me.Button12.TabIndex = 17
+        Me.Button12.Text = "WD"
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button11.Location = New System.Drawing.Point(146, 120)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(34, 21)
+        Me.Button11.TabIndex = 16
+        Me.Button11.Text = "FS"
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(295, 124)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(15, 14)
+        Me.CheckBox1.TabIndex = 15
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button10.ForeColor = System.Drawing.Color.Red
+        Me.Button10.Location = New System.Drawing.Point(214, 120)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(99, 21)
+        Me.Button10.TabIndex = 14
+        Me.Button10.Text = "mom walked in"
+        Me.Button10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button9.Location = New System.Drawing.Point(34, 120)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(61, 21)
+        Me.Button9.TabIndex = 13
+        Me.Button9.Text = "killingnigs"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button8.Location = New System.Drawing.Point(0, 120)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(35, 21)
+        Me.Button8.TabIndex = 12
+        Me.Button8.Text = "kkk"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button7.Location = New System.Drawing.Point(468, 120)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(80, 21)
+        Me.Button7.TabIndex = 11
+        Me.Button7.Text = "gore mixtapes"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button6.Location = New System.Drawing.Point(94, 120)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(53, 21)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "sound"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button5.Location = New System.Drawing.Point(547, 120)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(48, 21)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "soldat"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button4.Location = New System.Drawing.Point(594, 120)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(48, 21)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "rf"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button3.Location = New System.Drawing.Point(641, 120)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(48, 21)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "xnxx"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button2.Location = New System.Drawing.Point(688, 120)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(48, 21)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "gore"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Location = New System.Drawing.Point(735, 120)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(48, 21)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "e621"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button13.BackColor = System.Drawing.Color.Silver
+        Me.Button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button13.Location = New System.Drawing.Point(175, -1)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(25, 20)
+        Me.Button13.TabIndex = 7
+        Me.Button13.Text = "/\"
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button15.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button15.Location = New System.Drawing.Point(380, 120)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(45, 21)
+        Me.Button15.TabIndex = 19
+        Me.Button15.Text = "SPIN"
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button16.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button16.Location = New System.Drawing.Point(424, 120)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(45, 21)
+        Me.Button16.TabIndex = 20
+        Me.Button16.Text = "fek"
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Button17
+        '
+        Me.Button17.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button17.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button17.Location = New System.Drawing.Point(332, 120)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(49, 21)
+        Me.Button17.TabIndex = 21
+        Me.Button17.Text = "lynx"
+        Me.Button17.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Fuchsia
+        Me.ClientSize = New System.Drawing.Size(783, 550)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Panel2)
+        Me.Name = "Form1"
+        Me.Text = "sexy vb.net cp downloader"
+        Me.TopMost = True
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button16 As Button
+End Class
---
+++ RaidAssist/Form1.vb
@@ -0,0 +1,213 @@
+﻿Public Class Form1
+
+
+    ' Fill it with your path to your gore folder/kkk songs etc
+    Dim killingniggers As String = "z:\CSCHS\killingniggs.wav"
+    Dim kkk As String = "z:\CSCHS\kkk.wav"
+    Dim gorepath As String = "D:\Final\"
+    Dim none As String = "C:\nofile"
+    ' AUDIO MUST BE WAV 
+    ' C:\nofile must not exist in order to stop the audio
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label2.Text = DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss.ff") & " MM/DD/YYYY HH:MM:SS.ff UTC"
+    End Sub
+    Dim xuri, xkey As String
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        fetch()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.Hide()
+        PictureBox2.Hide()
+        fetch()
+        Me.TopMost = True
+        Me.TopLevel = True
+    End Sub
+
+    Private Sub Label1_TextChanged(
+ByVal sender As System.Object,
+ByVal e As System.EventArgs) _
+Handles Label1.TextChanged
+
+        Dim f As Font
+        Dim g As Graphics
+        Dim s As SizeF
+        Dim Faktor, FaktorX, FaktorY As Single
+
+        If Label1.Text.Length = 0 Then Return
+
+        g = Me.Label1.CreateGraphics
+        s = g.MeasureString(Label1.Text, Label1.Font)
+        g.Dispose()
+
+        FaktorX = Label1.Width / s.Width
+        FaktorY = Label1.Height / s.Height
+
+        If FaktorX > FaktorY Then
+            Faktor = FaktorY
+        Else
+            Faktor = FaktorX
+        End If
+
+        f = Label1.Font
+        Label1.Font = New Font(f.Name, f.SizeInPoints * Faktor)
+
+        Me.Text = Label1.Font.SizeInPoints.ToString
+        Label1.Text = Label1.Text
+
+    End Sub
+
+    Sub spinn()
+        If showspin Then
+            showspin = False
+            PictureBox1.Hide()
+            PictureBox2.Hide()
+        Else
+            showspin = True
+            PictureBox1.Show()
+            PictureBox2.Show()
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Try
+            My.Computer.Audio.Play(kkk)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Try
+            My.Computer.Audio.Play(killingniggers)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Try
+            My.Computer.Audio.Play(none)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Process.Start("https://www.e621.net/post/")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Process.Start("http://www.bestgore.com/all-the-gore/")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Process.Start("http://www.xnxx.com")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Process.Start("http://www.raidforums.com")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Process.Start("http://www.soldaten.cc")
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Process.Start(gorepath) ' 
+    End Sub
+    Dim showspin As Boolean = False
+    Dim momin As Boolean = False
+    Dim l1s As String
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If CheckBox1.Checked Then
+            CheckBox1.Checked = False
+            Button6_Click(Nothing, Nothing)
+            PictureBox1.Hide()
+            PictureBox2.Hide()
+
+            If momin Then
+                momin = False
+                Me.BackColor = Color.Fuchsia
+                Label1.Text = l1s
+                CheckBox1.Checked = True
+            Else
+                momin = True
+                l1s = Label1.Text
+                Me.BackColor = Color.Gray
+                Label1.Text = "mom"
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Me.FormBorderStyle = FormBorderStyle.Sizable
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If Panel2.Visible = True Then
+            Panel2.Visible = False
+            Button13.Text = "V"
+        Else
+            Panel2.Visible = True
+            Button13.Text = "/\"
+        End If
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If CheckBox1.Checked = False Then
+            Me.Close()
+        End If
+    End Sub
+
+    Sub fetch()
+        Try
+            Dim s() As String = Split(My.Computer.Clipboard.GetText, ";")
+            xuri = s(0)
+            xkey = s(1)
+            Me.Text = xuri
+            Label3.Text = xkey
+        Catch ex As Exception
+            Me.Text = "Fetching failed. "
+
+        End Try
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        Label1.Text = InputBox("Autosize", "Edit Text", Label1.Text)
+    End Sub
+
+    Private Sub Button14_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Button14.MouseDoubleClick
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        spinn()
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Process.Start("https://e621.net/post/show/663873")
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Process.Start("https://e621.net/post/show/707571")
+    End Sub
+
+    Private Sub Label3_MouseClick(sender As Object, e As MouseEventArgs) Handles Label3.MouseClick
+        If e.Button = MouseButtons.Right Then
+            Label3.Text = InputBox("Fixed Size", "Edit Text/Enter Streamkey", Label3.Text)
+        End If
+
+    End Sub
+End Class
